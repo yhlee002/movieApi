de0991406c49e12e5aee677552da05dcb60a8500
yhlee <xxxoxxo002@gmail.com>
2024-04-25 18:00:27 +0900

    Naver movie API의 종료로 TMDB API 관련 클래스 추가

---
+++ src/main/java/com/portfolio/demo/project/vo/tmdb/GenreVO.java
@@ -0,0 +1,6 @@
+package com.portfolio.demo.project.vo.tmdb;
+
+public class GenreVO {
+    private String id;
+    private String name;
+}
---
+++ src/main/java/com/portfolio/demo/project/vo/tmdb/ImageVO.java
@@ -0,0 +1,11 @@
+package com.portfolio.demo.project.vo.tmdb;
+
+public class ImageVO {
+    private double aspectRatio; // aspect_ratio
+    private double height;
+    private String iso_639_1;
+    private String filePath; // file_path
+    private double voteAverage; // vote_average
+    private int voteCount; // vote_count
+    private double width;
+}
