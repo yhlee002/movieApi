d9af46fc5416f8c5780dd9ffaeae135624a4b85c
yhlee <xxxoxxo002@gmail.com>
2024-05-01 01:19:38 +0900

    CommentMovRepository 관련 Test & TestDataBuilder 작성, 불필요한 메서드 제거

---
+++ src/test/java/com/portfolio/demo/project/model/CommentMoveTestDataBuilder.java
@@ -0,0 +1,16 @@
+package com.portfolio.demo.project.model;
+
+import com.portfolio.demo.project.entity.comment.CommentMov;
+import com.portfolio.demo.project.entity.member.Member;
+
+public class CommentMoveTestDataBuilder {
+
+    public static CommentMov.CommentMovBuilder noWayUpComment(Member member) {
+        return CommentMov
+                .builder()
+                .writer(member)
+                .content("test comment")
+                .movieNo(1096197L) // No Way Up
+                .rating(4);
+    }
+}
