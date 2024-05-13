8b56c3fc5e1d99b5a2c367c80f2212fa75f93ef0
yhlee <xxxoxxo002@gmail.com>
2024-05-13 15:27:48 +0900

    영화 API 관련 VO 추가, 불필요한 클래스 제거

---
+++ src/main/java/com/portfolio/demo/project/vo/kmdb/VodVO.java
@@ -0,0 +1,9 @@
+package com.portfolio.demo.project.vo.kmdb;
+
+import lombok.Getter;
+
+@Getter
+public class VodVO {
+    private String vodClass; // VOD 구분
+    private String vodUrl; // VOD URL
+}
---
+++ src/main/java/com/portfolio/demo/project/vo/tmdb/TmdbMovieVO.java
@@ -0,0 +1,24 @@
+package com.portfolio.demo.project.vo.tmdb;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Builder
+@Getter @ToString
+public class TmdbMovieVO {
+    private boolean adult; // 관람 등급 성인 여부
+    private String backdrop_path; // backdrop_path
+    private int[] genre_ids; // genre_ids // 장르 아이디 목록
+    private String id;
+    private String original_language; // original_language
+    private String original_title; // original_title
+    private String overview;
+    private double popularity;
+    private String poster_path; // poster_path
+    private String release_date; // release_date
+    private String title;
+    private boolean video;
+    private double vote_average; // vote_average
+    private int vote_count; // vote_count
+}
