7377f86c4c725711f14458519b47653a1c1d718f
yhlee <xxxoxxo002@gmail.com>
2024-05-13 15:27:48 +0900

    영화 API 관련 VO 추가, 불필요한 클래스 제거

---
+++ src/main/java/com/portfolio/demo/project/vo/kmdb/StaffVO.java
@@ -0,0 +1,12 @@
+package com.portfolio.demo.project.vo.kmdb;
+
+import lombok.Getter;
+
+@Getter
+public class StaffVO {
+    private String staffNm; // 스텝 이름
+    private String staffRoleGroup; //  스텝 크레딧명
+    private String StaffRole; // 스텝 배역
+    private String StaffEtc; // 스텝 기타
+    private String StaffId; // 스텝 등록 번호
+}
