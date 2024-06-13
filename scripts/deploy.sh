d8f785df9d4764c76bc7c7df7de2e0f9efd46194
yhlee <xxxoxxo002@gmail.com>
2024-06-13 22:32:42 +0900

    deploy.sh 배포 경로 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-BUILD_JAR=$(ls /home/ec2-user/action/api/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/app/api/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/action/api/deploy.log
+echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/app/api/deploy.log
 
-echo ">>> build 파일 복사" >> /home/ec2-user/action/deploy.log
-DEPLOY_PATH=/home/ec2-user/action/
+echo ">>> build 파일 복사" >> /home/ec2-user/app/deploy.log
+DEPLOY_PATH=/home/ec2-user/app/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/action/deploy.log
+echo ">>> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/app/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/action/deploy.log
+  echo ">>> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/app/deploy.log
 else
   echo ">>> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -21,5 +21,5 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo ">>> DEPLOY_JAR 배포"    >> /home/ec2-user/action/deploy.log
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/action/deploy_err.log &
+echo ">>> DEPLOY_JAR 배포"    >> /home/ec2-user/app/deploy.log
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/app/deploy_err.log &
