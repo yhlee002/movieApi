66cdffd22548ecde85aa47f43a68b6f8956b825c
yhlee <xxxoxxo002@gmail.com>
2024-07-05 22:41:51 +0900

    del_yn 컬럼 추가 sql 파일 제거

--- db/migration/V2__add_del_yn_column.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE board_imp ADD COLUMN del_yn varchar(10);
-
-ALTER TABLE board_notice ADD COLUMN del_yn varchar(10);
-
-ALTER TABLE member ADD COLUMN del_yn varchar(10);
-
-ALTER TABLE comment_imp ADD COLUMN del_yn varchar(10);
