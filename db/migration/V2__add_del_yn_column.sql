f673950eced8a64f782804c42c9053ec0e2ada0e
yhlee <xxxoxxo002@gmail.com>
2024-07-05 11:46:44 +0900

    flyway sql 파일의 if exist 구문 삭제

--- db/migration/V2__add_del_yn_column.sql
+++ db/migration/V2__add_del_yn_column.sql
@@ -1,7 +1,7 @@
-ALTER TABLE IF EXISTS board_imp ADD COLUMN del_yn varchar(10);
+ALTER TABLE board_imp ADD COLUMN del_yn varchar(10);
 
-ALTER TABLE IF EXISTS board_notice ADD COLUMN del_yn varchar(10);
+ALTER TABLE board_notice ADD COLUMN del_yn varchar(10);
 
-ALTER TABLE IF EXISTS member ADD COLUMN del_yn varchar(10);
+ALTER TABLE member ADD COLUMN del_yn varchar(10);
 
-ALTER TABLE IF EXISTS comment_imp ADD COLUMN del_yn varchar(10);
+ALTER TABLE comment_imp ADD COLUMN del_yn varchar(10);
